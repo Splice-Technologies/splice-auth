ca709adb3f32528d94bdc950f78b185189ad7634
Borjan Gjorovski <borjan.gjorovski@outlook.com>
2021-03-28 21:07:20 +0200

    Fuck tests (Should do them later)

--- users/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from .utils import UsersUtils
-
-
-class UserTestCase(TestCase):
-    def test_message_response_generator(self):
-        response = UsersUtils.generate_message_response('Testing message', success=True)
-        print(response.data['success'])
